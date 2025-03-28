69a3c02b8ded636bfbb1fc6931af8eaf59602ee0
hossamhassan16 <hh1679@fayoum.edu.eg>
2025-03-28 06:18:21 +0200

    adding data to firebase and create future builder chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,60 +1,88 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
   static String id = "ChatPage";
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/logo-removebg-preview.png",
-              height: 50,
-            ),
-            Text(
-              "chat",
-              style: TextStyle(
-                color: Colors.white,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // print(snapshot.data!.docs[2]["message"]);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/logo-removebg-preview.png",
+                    height: 50,
+                  ),
+                  Text(
+                    "chat",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
+              centerTitle: true,
             ),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(itemBuilder: (context, builder) {
-              return ChatBubble();
-            }),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Send Message",
-                suffixIcon: Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, builder) {
+                    return ChatBubble();
+                  }),
                 ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({
+                        "message": data,
+                      });
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Send Message",
+                      suffixIcon: Icon(
+                        Icons.send,
+                        color: kPrimaryColor,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: BorderSide(
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return Text("Loading...");
+        }
+      },
     );
   }
 }
